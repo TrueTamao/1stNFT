41ca03b5c268f06cf8ff11513176492139531f06
TrueTamao <tamaoserizawa123@gmail.com>
2021-11-26 23:54:09 +0700

    add advanced collectible

---
+++ scripts/advanced_collectible/deploy_n_create.py
@@ -0,0 +1,25 @@
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract, fund_with_link
+from brownie import AdvancedCollectible, network, config
+
+
+def deploy_n_create():
+    account = get_account()
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
+    );
+
+    fund_with_link(advanced_collectible.address);
+
+    creating_tx = advanced_collectible.createCollectible();
+    creating_tx.wait(1);
+    print("New Token has been created");
+    
+    return advanced_collectible
+
+
+def main():
+    deploy_n_create()
